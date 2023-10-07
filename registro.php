19839a5a0260758be40159bd5a29e6b5f5caed02
Hugo de la Hoz <hugo.dlhp@gmail.com>
2023-10-07 23:02:08 +0200

    solucion problema de registro de usuarios

--- registro.php
+++ registro.php
@@ -1,8 +1,5 @@
 <?php
 
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -17,19 +14,18 @@
     }
 
     // Obtener datos del formulario y evitar inyección de SQL
-    $nombre = mysqli_real_escape_string($conn, $_POST['nombre']);
-    $apellidos = mysqli_real_escape_string($conn, $_POST['apellidos']);
-    $usuario = mysqli_real_escape_string($conn, $_POST['nuevo-usuario']);
-    $contrasena = mysqli_real_escape_string($conn, $_POST['nueva-contraseña']); 
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $usuario = $_POST['nuevo-usuario'];
+    $contraseña = $_POST['nueva-contraseña']; 
 
-    // Hashear la contraseña antes de guardarla en la base de datos (seguridad)
-    $contrasena_hasheada = password_hash($contrasena, PASSWORD_DEFAULT);
 
     // Insertar datos en la base de datos
-    $sql = "INSERT INTO usuarios (nombre, apellidos, usuario, contrasena) VALUES ('$nombre', '$apellidos', '$usuario', '$contrasena_hasheada')";
+    $sql = "INSERT INTO usuarios (nombre, apellidos, usuario, contrasena) VALUES ('$nombre', '$apellidos', '$usuario', '$contraseña')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Registro exitoso";
+        header("Location: login.html");
+        exit();
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
--- tablas.sql
+++ tablas.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS usuarios;
+
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
